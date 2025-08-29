5c859264acfaab4bf2fbaa209f7867508bfff77a
Le Hoang Thinh <thinhphuongdong3@gmail.com>
2025-08-29 22:22:55 +0700

    Fix to for running CI build

--- src/main/java/com/pfh/user/actuator/ApplicationHealthIndicator.java
+++ src/main/java/com/pfh/user/actuator/ApplicationHealthIndicator.java
@@ -1,4 +1,4 @@
-package com.pfh.user.infrastructure.health;
+package com.pfh.user.actuator;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
