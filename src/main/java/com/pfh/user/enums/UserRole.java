09b7d11b400cad7898df4637d10db8b418c577cc
Le Hoang Thinh <thinhphuongdong3@gmail.com>
2025-09-04 10:38:30 +0700

    [skip ci] Add UserRole to the UserEntity

---
+++ src/main/java/com/pfh/user/enums/UserRole.java
@@ -0,0 +1,13 @@
+package com.pfh.user.enums;
+
+public enum UserRole {
+    // External user roles
+    NORMAL_USER,
+
+    // Third-party
+    AUDITOR,
+
+    // Internal user roles
+    ADMIN
+
+}
